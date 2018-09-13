403c4042ddad4ad735137a3cd4031b6c1b887e0c
Antar Ahmed <aantar@SQLI3075.SQLI>
2018-09-13 17:05:24 +0200

    adding state

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 class Welcome extends Component{
+    constructor(){
+        super();
+        this.state = {
+            count : 0 
+        }
+       
+    }
+
+    addOne(){
+      this.setState({
+          count : this.state.count + 1
+      })
+    }
+
     render(){
         return (
+         <div>
          <h1>WELCOME {this.props.name}</h1>
+         <p>Mon compteur: {this.state.count}</p>
+         <button onClick = {() => this.addOne()} >AddOne</button>
+         </div>   
         );
     }
 }
